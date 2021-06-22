6e32c7246116bdba3c19c6253cf57110cbb16789
Karol Przybylski <karol.przybylski@nokia.com>
2021-06-22 16:30:05 +0200

    WIP thanos.hpp seen in cmake,

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,12 +1,13 @@
 #include "gtest/gtest.h"
 
-#include "../main.cpp"
+#include "thanos.hpp"
 
 #include <iostream>
 
 TEST(SquareRootTest, PositiveNos) {
     EXPECT_EQ (1,1);
-    int random_index = get_random_index(50);
+    int val = 50;
+    int random_index = get_random_index(val);
     std::cout<< "index :" << random_index << std::endl;
 }
 
